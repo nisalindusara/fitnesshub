7cc0733c3c27a46363bcbd59a71c880246107cbf
Nisal <91654417+nisalindusara@users.noreply.github.com>
2026-08-18 21:45:20 +0530

    chore: initialize database config, PDO and base model

--- config/database.php
+++ config/database.php
@@ -1,1 +1,8 @@
 <?php
+return [
+    'host'     => 'localhost',
+    'dbname'   => 'fitnesshub',
+    'username' => 'root',
+    'password' => '', // default XAMPP MySQL has no password
+    'charset'  => 'utf8mb4',
+];
