1a8112f3662ee3ee7957a09269632504685efe8d
Nisal <91654417+nisalindusara@users.noreply.github.com>
2026-08-27 08:34:10 +0530

    fix: orders route in the navbar

--- app/views/layouts/staff-layout.php
+++ app/views/layouts/staff-layout.php
@@ -14,7 +14,7 @@ $navItems = [
         ['label' => 'Add Payment', 'icon' => 'plus-circle', 'route' => '/payments/add', 'permission' => 'add_payment'],
     ]],
     ['section' => 'eCommerce', 'label' => 'Store', 'icon' => 'box', 'route' => '/store', 'permission' => 'manage_inventory'],
-    ['section' => 'eCommerce', 'label' => 'Orders', 'icon' => 'package', 'route' => '/orders', 'permission' => 'manage_orders'],
+    ['section' => 'eCommerce', 'label' => 'Orders', 'icon' => 'package', 'route' => '/portal/orders', 'permission' => 'manage_orders'],
 ];
 
 $visibleNav = [];
