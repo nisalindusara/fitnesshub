89d3217a0db0d0802cf904c85db91f0e409c3624
Nisal <91654417+nisalindusara@users.noreply.github.com>
2026-09-25 12:33:55 +0530

    add history.back() for back-btn in category screen

--- app/views/ecommerce_module/category_screen.php
+++ app/views/ecommerce_module/category_screen.php
@@ -331,7 +331,7 @@
 
 <div class="app-container">
     <header class="header">
-        <button class="header-btn">
+        <button class="header-btn" onclick="history.back()">
             <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                 <path d="M19 12H5M12 19L5 12L12 5" stroke="#1C1C1C" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
             </svg>
