0dca3aa2116ace24cb2faf020bbeb984e8c53721
Nisal <91654417+nisalindusara@users.noreply.github.com>
2026-09-26 10:46:48 +0530

    Create the rest of the pages from the member profile

--- app/controllers/member/MemberScreenController.php
+++ app/controllers/member/MemberScreenController.php
@@ -6,4 +6,24 @@ class MemberScreenController extends Controller
     {
         $this->render('member/personal-details', 'member-layout');
     }
+    public function showMemberOrderHistoryScreen(): void
+    {
+        $this->render('member/order-history', 'member-layout');
+    }
+    public function showMemberOrderDetailsScreen(): void
+    {
+        $this->render('member/order-detail', 'member-layout');
+    }
+    public function showMemberPaymentHistoryScreen(): void
+    {
+        $this->render('member/payment-history', 'member-layout');
+    }
+    public function showMemberNotificationSettingsScreen(): void
+    {
+        $this->render('member/notification-settings', 'member-layout');
+    }
+    public function showMemberPrivacyDataScreen(): void
+    {
+        $this->render('member/privacy-data', 'member-layout');
+    }
 }
